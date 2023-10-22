de7a0277495ed33f5121ba1b12f6309045f3ce4e
ccweerasinghe1994 <ccweerasinghe1994@gmail.com>
2023-10-22 13:15:37 +0530

    Theme

---
+++ components/shared/navbar/NavBar.tsx
@@ -0,0 +1,51 @@
+import { FC } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+
+type TNavBar = {};
+const NavBar: FC<TNavBar> = () => {
+  return (
+    <nav
+      className={
+        "flex-between background-light900_dark200 fixed z-50 flex w-full gap-5  p-6 shadow-light-300 dark:shadow-none sm:px-12"
+      }
+    >
+      <Link href={"/"} className={"flex items-center gap-1"}>
+        <Image
+          src={"/assets/images/site-logo.svg"}
+          width={23}
+          height={23}
+          alt={"StackFlow"}
+        />
+        <p
+          className={
+            "h2-bold min-sm:hidden font-spaceGrotesk text-dark-100 dark:text-light-900"
+          }
+        >
+          Stack <span className={"text-primary-500"}>Flow</span>
+        </p>
+      </Link>
+      {/*  GLOBAL SEARCH COMPONENT */}
+      <div className={"flex-between gap-5"}>
+        Theme
+        <SignedIn>
+          <UserButton
+            appearance={{
+              elements: {
+                avatarBox: "h-10 w-10",
+              },
+              variables: {
+                colorPrimary: "#ff7000",
+              },
+            }}
+            afterSignOutUrl="/"
+          />
+        </SignedIn>
+        Mobile Navigation
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
