74399e62232f8ff447c8c5f7099b73f40c5176d4
ccweerasinghe1994 <ccweerasinghe1994@gmail.com>
2023-10-28 14:05:45 +0530

    next version upgrade

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -10,7 +10,7 @@ export async function getUserById(params: TGetUserById): Promise<IUser> {
   try {
     const { userId } = params;
     await connectToDatabase();
-    const user = await User.findOne({ clerkId: "123" });
+    const user = await User.findOne({ clerkId: userId });
     return user;
   } catch (error) {
     console.error(error);
