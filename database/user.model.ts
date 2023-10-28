f4cfd1970daad4c0caf58f49542031986b8483ab
ccweerasinghe1994 <ccweerasinghe1994@gmail.com>
2023-10-28 08:01:20 +0530

    Creating a Tag,User Model

---
+++ database/user.model.ts
@@ -0,0 +1,76 @@
+import type { Document, ObjectId } from "mongoose";
+import { model, models, Schema } from "mongoose";
+
+export interface IUser extends Document {
+  clearId: string;
+  name: string;
+  username: string;
+  email: string;
+  password?: string;
+  bio?: string;
+  picture: string;
+  location?: string;
+  portfolioWebsite?: string;
+  reputation?: number;
+  saved: ObjectId[];
+  joinedAt: Date;
+}
+
+const UserSchema = new Schema<IUser>({
+  clearId: {
+    type: String,
+    require: true,
+  },
+  name: {
+    type: String,
+    require: true,
+  },
+  username: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    require: true,
+  },
+  password: {
+    type: String,
+  },
+  bio: {
+    type: String,
+    required: false,
+  },
+  picture: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: false,
+  },
+  portfolioWebsite: {
+    type: String,
+    required: false,
+  },
+  reputation: {
+    type: Number,
+    required: false,
+    default: 0,
+  },
+  saved: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Question",
+    },
+  ],
+  joinedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = models.User || model<IUser>("User", UserSchema);
+
+export default User;
