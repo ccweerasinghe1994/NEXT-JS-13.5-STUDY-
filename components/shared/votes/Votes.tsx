d31a868fa0ad9a2058847ea1eeffa1b967a1172f
ccweerasinghe1994 <ccweerasinghe1994@gmail.com>
2023-11-01 09:39:54 +0530

    Create Votes UI

---
+++ components/shared/votes/Votes.tsx
@@ -0,0 +1,87 @@
+"use client";
+import { FC } from "react";
+import { ObjectId } from "mongoose";
+import Image from "next/image";
+import { formatNumber } from "@/lib/utils";
+
+type TVotesProps = {
+  type: "question" | "answer";
+  itemId: ObjectId;
+  userId: ObjectId;
+  upvotes: number;
+  hasUpVoted: boolean;
+  downVotes: number;
+  hasDownVoted: boolean;
+  hasSaved: boolean;
+};
+const Votes: FC<TVotesProps> = ({
+  hasUpVoted,
+  upvotes,
+  downVotes,
+  hasDownVoted,
+  itemId,
+  type,
+  hasSaved,
+  userId,
+}) => {
+  const handleSave = () => {};
+  const handleVote = (type: "upvote" | "downVote") => {};
+  return (
+    <div className={"flex gap-5"}>
+      <div className="flex-center gap-2.5">
+        <div className="flex-center gap-1.5">
+          <Image
+            src={
+              hasUpVoted
+                ? "/assets/icons/upvoted.svg"
+                : "/assets/icons/upvote.svg"
+            }
+            alt={"upvote icon"}
+            width={18}
+            height={18}
+            className={"cursor-pointer"}
+            onClick={() => handleVote("upvote")}
+          />
+          <div className="background-light700_dark400 flex min-w-[18px] items-center rounded-sm p-1">
+            <p className="subtle-medium text-dark400_light900">
+              {formatNumber(upvotes)}
+            </p>
+          </div>
+        </div>
+        <div className="flex-center gap-1.5">
+          <Image
+            src={
+              hasDownVoted
+                ? "/assets/icons/downvoted.svg"
+                : "/assets/icons/downvote.svg"
+            }
+            alt={"upvote icon"}
+            width={18}
+            height={18}
+            className={"cursor-pointer"}
+            onClick={() => handleVote("downVote")}
+          />
+          <div className="background-light700_dark400 flex min-w-[18px] items-center rounded-sm p-1">
+            <p className="subtle-medium text-dark400_light900">
+              {formatNumber(downVotes)}
+            </p>
+          </div>
+        </div>
+      </div>
+      <Image
+        src={
+          hasSaved
+            ? "/assets/icons/start-filled.svg"
+            : "/assets/icons/star-red.svg"
+        }
+        alt={"upvote icon"}
+        width={18}
+        height={18}
+        className={"cursor-pointer"}
+        onClick={handleSave}
+      />
+    </div>
+  );
+};
+
+export default Votes;
